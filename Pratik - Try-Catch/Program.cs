c40c8daf0ebadbaa964b0e128c0fd7db52fdc4fd
Aslıhan ESEN <aslyqc14@gmail.com>
2024-10-07 00:41:11 +0300

    Proje dosyası ekle.

---
+++ Pratik - Try-Catch/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            // Kullanıcıdan bir sayı girmesini istiyoruz
+            Console.Write("Lütfen bir sayı giriniz: ");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+
+            // Sayının karesini hesaplayıp ekrana yazdırıyoruz
+            int kare = sayi * sayi;
+            Console.WriteLine($"Girdiğiniz sayının karesi: {kare}");
+        }
+        catch (FormatException)
+        {
+            // Kullanıcı geçersiz bir giriş yaparsa hata mesajı gösteriyoruz
+            Console.WriteLine("Geçersiz giriş! Lütfen bir sayı giriniz.");
+        }
+    }
+}
+
---
+++ Pratik - Try-Catch/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("Pratik - Try-Catch")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Pratik - Try-Catch")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("5680873c-9f9c-4d7f-8692-22aed7b4ccdf")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
